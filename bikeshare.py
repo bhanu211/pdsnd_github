0b634fe366951fb4dd168f12645afc087903fa81
Bhanu Muvva <bmuvva@qualcomm.com>
2019-03-11 12:08:53 -0400

    Add comments to bikeshare.py

--- bikeshare.py
+++ bikeshare.py
@@ -6,7 +6,9 @@ CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
 
-
+#Add comments line 1
+#Add comments line 2
+#Add comments line 3
 def get_filters():
     """
     Asks user to specify a city, month, and day to analyze.
